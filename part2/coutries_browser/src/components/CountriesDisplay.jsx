cc1c540d24087fe0326ed0506cb477fc1c26a38d
Leti <leticieouthierova@seznam.cz>
2023-02-07 10:09:36 +0100

    show selected country button

--- part2/coutries_browser/src/components/CountriesDisplay.jsx
+++ part2/coutries_browser/src/components/CountriesDisplay.jsx
@@ -1,26 +1,39 @@
 import { CountryInfo } from "./CountryInfo"
+import { useState } from "react"
+
 
 export const CountriesDisplay = ({countriesToShow}) => {
-    if (countriesToShow.length === 1) {
-      return (
-        <CountryInfo country={countriesToShow[0]}/>
-      )  
-    }
-    if (countriesToShow.length < 11) {
-      return (
-        <div>
-          {countriesToShow.map(country => (      
-            <div key={country.name.common}>
-              <p>{country.name.common}</p>
-            </div>  
-          ))}
-        </div>
-      )  
-    } else {
-      return (
-        <div>
-          <p>Too many matches, specify another filter</p>
-        </div>
-      )  
-    }     
+  const [selectedCountry, setSelectedCountry] = useState([])
+  const [buttonClicked, setButtonClicked] = useState(false)
+
+  const handleChange = (country) => {
+    setSelectedCountry(country)
+    setButtonClicked(true)
   }
+
+
+  if (buttonClicked === true) {
+    console.log(selectedCountry)
+    return (
+      <CountryInfo country={selectedCountry}/>
+    )
+  }  
+  if (countriesToShow.length < 11) {
+    return (
+      <div>
+        {countriesToShow.map(country => (      
+          <div key={country.name.common}>
+            <p>{country.name.common}</p>
+            <button onClick={() => handleChange(country)}>show</button>
+          </div>  
+        ))}
+      </div>
+    )  
+  } else {
+    return (
+      <div>
+        <p>Too many matches, specify another filter</p>
+      </div>
+    )  
+  }     
+}
